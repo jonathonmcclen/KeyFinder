524de47c02982f8099a5c90ca6aad0a5dc0b0ba9
Josh <joshdaley7@gmail.com>
2020-08-19 01:16:45 -0400

    changed visuals and added chord list logic

--- Logic.js
+++ Logic.js
@@ -1,11 +1,22 @@
 var pianoKeyElements;
+var originalHTML;
 var noteNames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
 
 function getPianoKeyElements() {
     pianoKeyElements = document.querySelectorAll('.piano-key');
+    
+    originalHTML = {};
+    for (let elem of pianoKeyElements) {
+        originalHTML[elem.id] = elem.firstChild.innerHTML;
+    }
 }
 
+function addInitialGreyOut() {
+    for (let elem of pianoKeyElements) {
+        elem.firstChild.innerHTML += "<span class=\"circle grey-circle\"></span>";
+    }
+}
 
 function updatePiano() {
 	
@@ -16,7 +27,7 @@ function updatePiano() {
         return;
     
 
-    var notesInScale = []
+    var notesInScale = [];
     
 	
     if (scaleType == 'major') {
@@ -41,13 +52,32 @@ function updatePiano() {
     notesInScale.push(noteNames[currentIndex % 12]);
     
     for (let pianoKey of pianoKeyElements) {
-        if (notesInScale.includes(pianoKey.firstChild.innerHTML)) {
-            if (pianoKey.className.includes(' grayed-out'))
-                pianoKey.className = pianoKey.className.replace(' grayed-out', '');
+        if (notesInScale.includes(originalHTML[pianoKey.id])) {
+            pianoKey.firstChild.innerHTML = originalHTML[pianoKey.id] + "<span class=\"circle green-circle\"></span>";
+            pianoKey.firstChild.className = 'label';
         }
-        else if (!pianoKey.className.includes(' grayed-out')) {
-            pianoKey.className += ' grayed-out';
+        else {
+            pianoKey.firstChild.innerHTML = ' X' + "<span class=\"circle red-circle\"></span>";
+            pianoKey.firstChild.className = 'x';
         }
+    }
+    
+    updateChordList(notesInScale, scaleType);
+    
+}
+
+function updateChordList(notesInScale, scaleType) {
+    let major_order = ['maj', 'min', 'min', 'maj', 'maj', 'min', 'dim'];
+    let minor_order = ['min', 'dim', 'maj', 'min', 'min', 'maj', 'maj'];
+    
+    if (scaleType == 'major')
+        var order = major_order;
+    else if (scaleType == 'minor')
+        var order = minor_order;
+    else return;
+
+    for (let i = 1; i < 8; i++) {
+        document.getElementById('chord' + i).innerHTML = notesInScale[i - 1] + order[i - 1];
     }
     
 }
@@ -55,4 +85,5 @@ function updatePiano() {
 
 window.onload = function() {
     getPianoKeyElements();
+    addInitialGreyOut();
 }
